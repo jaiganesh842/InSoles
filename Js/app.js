ede00316c45d296ded8c4e74397d6c4e4d4f0aa8
jaiganesh <jaiganeshkn842@gmail.com>
2022-09-21 17:21:54 +0530

    nav bar completed

---
+++ Js/app.js
@@ -0,0 +1,21 @@
+var i = 0;
+var images = [];
+var time = 4000;
+
+images[0]='Images/crocs.jpg'
+images[1]='Images/slippers.jpg'
+images[2]='Images/womenshoes.jpg'
+
+function changeimg(){
+    document.slide.src = images[i];
+
+    if(i < images.length -1){
+        i++;
+    }else{
+        i=0;
+    }
+
+    setTimeout("changeimg()",time);
+}
+
+window.onload = changeimg;
